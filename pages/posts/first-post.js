2ba4f6e3be063849529be670fc8cbc6ef97b00df
Yogesh Amane <yogeshamane@WAAREE.LOCAL>
2023-05-09 13:54:31 +0530

    added _app.js - top level react component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,9 +2,10 @@ import React from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 import Script from 'next/script'
+import Layout from '../../components/layout'
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
     <Head>
       <title>First Post</title>
       <Script
@@ -14,14 +15,14 @@ export default function FirstPost() {
         }
       />
     </Head>
-        <h1>First Post</h1>
-        <h2><Link href="/">Back to Home</Link></h2>
+    <h1>First Post</h1>
+    <h2><Link href="/">Back to Home</Link></h2>
     
     <Script>
       console.log("hello");
       {/* alert("Hi"); */}
     </Script>
-    </>
+    </Layout>
 
     
   )
